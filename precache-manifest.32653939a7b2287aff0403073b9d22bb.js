296f0586550e4b2c0236f7f6f7b65f691311a8c5
Your Name <you@example.com>
2019-06-09 17:40:09 +1200

    Updates

---
+++ precache-manifest.32653939a7b2287aff0403073b9d22bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467e1a141239a8ae58b2fe363de2a4bd",
+    "url": "/react_wp/index.html"
+  },
+  {
+    "revision": "85b8a32daa4689e35fc1",
+    "url": "/react_wp/static/css/main.ceab8a2c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_wp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "85b8a32daa4689e35fc1",
+    "url": "/react_wp/static/js/main.ce868594.chunk.js"
+  },
+  {
+    "revision": "82e4cc90c5f5a720de93",
+    "url": "/react_wp/static/js/runtime~main.d476ed89.js"
+  }
+]);
